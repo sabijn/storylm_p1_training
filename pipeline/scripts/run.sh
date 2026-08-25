2c4f0814fa67fc89724924ea7b71652b391e0f2a
Sabijn Perdijk <sperdijk@int6.local.snellius.surf.nl>
2026-08-25 12:48:17 +0200

    second run tokenizer experiment

--- pipeline/scripts/run.sh
+++ pipeline/scripts/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=storylm-base-32k
+#SBATCH --job-name=storylm-base-32k-run2
 #SBATCH --partition=gpu_h100
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=120G
-#SBATCH --time=05:00:00
+#SBATCH --time=03:00:00
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.out
 
@@ -15,11 +15,6 @@ export CODE_TEMP_DIR="/scratch-shared/$USER/storylm_p1_training"
 # Create temp running dir
 mkdir -p "$CODE_TEMP_DIR"
 rsync -a --exclude='.git' "$WORKING_DIR/" "$CODE_TEMP_DIR/"
-
-# Create output dir
-export RUN_NAME="storylm-base"
-export OUTPUT_DIR="$CODE_TEMP_DIR/checkpoints/$RUN_NAME/"
-mkdir -p "$OUTPUT_DIR"
 
 # Switch working dir
 cd "$CODE_TEMP_DIR"
